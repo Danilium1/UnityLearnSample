70089481894fb28f7a1f26cb561f4b0b1c2baa37
Danilium1 <vtoroidrug@gmail.com>
2026-02-20 03:30:02 +0700

    my part of house
    сделал свою часть домика, первый раз комичу его

---
+++ Assets/LearnMaterials 1/test_for_zaitsev.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_for_zaitsev : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Мой первый скрипт работает!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
